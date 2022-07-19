4d3c34181e72fc60326dc563bbade25cd88170f6
Diego Guillen <dguillen@rhema.org>
2022-07-19 08:46:51 -0500

    g

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import Form_Asistentes from './App'
 import 'bootstrap/dist/css/bootstrap.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <App />
+    <Form_Asistentes />
   </React.StrictMode>,
 )
